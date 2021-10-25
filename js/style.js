884241f31f6705f1821c022f52f02cc6b400b09b
Antoniok2 <antoniopiazza18@gmail.com>
2021-10-25 17:12:46 +0200

    aggiunte condizioni

--- js/style.js
+++ js/style.js
@@ -1,16 +1,33 @@
 const squareCont = document.getElementById("containersquare")
 
 for (let i = 1; i <= 100; i++){
-    squareCont.innerHTML += 
-    `<div class="square">
-        ${i}
-    </div>`
 
-    if (i % 3 === 0) {
+    // SE DIVISIBILE PER 3 E PER 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        squareCont.innerHTML += 
+        `<div class="square squarefizzbuzz">
+            FizzBuzz
+        </div>`
+
+    // SE DIVISIBILE PER 3
+    } else if (i % 3 === 0) {
+        squareCont.innerHTML += 
+        `<div class="square squarefizz">
+            Fizz
+        </div>`;
+
+    // SE DIVISIBILE PER 5
+    }  else if (i % 5 === 0) {
+        squareCont.innerHTML += 
+        `<div class="square squarebuzz">
+            Buzz
+        </div>`
+    
+    // ALTRIMENTI
+    } else {
+        squareCont.innerHTML += 
         `<div class="square">
-        ${"fizz"}
-    </div>`        
-
+            ${i}
+        </div>`
     }
-
 }
